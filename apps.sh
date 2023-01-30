6b660c165514e37addb388224e87592e0091efef
ReevesA1 <101371526+ReevesA1@users.noreply.github.com>
2023-01-30 16:57:28 -0500

    Create apps.sh

---
+++ apps.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+#Browsers
+flatpak install flathub org.mozilla.firefox -y
+flatpak install flathub com.microsoft.Edge -y
+
+#Media
+flatpak install flathub com.spotify.Client -y
+flatpak install flathub tv.plex.PlexDesktop -y #Client not server
+flatpak install flathub org.gnome.Podcasts -y
+flatpak install flathub de.haeckerfelix.Shortwave -y
+
+#Locale Media Players
+flatpak install flathub org.gnome.Totem -y #for videos
+flatpak install flathub com.github.johnfactotum.Foliate -y #for books and comics
+flatpak install flathub io.bassi.Amberol -y #for music
+
+
+#Security
+flatpak install flathub com.bitwarden.desktop -y
+
+#Utilities
+flatpak install flathub org.videolan.VLC -y
+flatpak install flathub com.github.tchx84.Flatseal -y
+flatpak install flathub app.drey.Warp -y
+flatpak install flathub org.gabmus.whatip -y
+flatpak install flathub com.github.unrud.VideoDownloader -y
+flatpak install flathub org.godotengine.Godot -y
+flatpak install flathub com.github.debauchee.barrier -y
+
+
+#Social
+flatpak install flathub org.ferdium.Ferdium -y
+flatpak install flathub com.gitlab.newsflash -y
+
+#Productivity
+flatpak install flathub com.todoist.Todoist -y
+flatpak install flathub com.github.marktext.marktext -y
+
+#Game Launchers
+flatpak install flathub com.heroicgameslauncher.hgl -y
+flatpak install flathub net.lutris.Lutris -y
+flatpak install flathub net.davidotek.pupgui2 -y #protonUp-Qt
+flatpak install flathub io.github.hmlendea.geforcenow-electron -y
+flatpak install flathub com.usebottles.bottles -y
+
+#Steamdeck exclusives
+flatpak install flathub com.steamdeckrepo.manager -y # Install boot videos to your Steam Deck using Steam Deck Repo website API.
